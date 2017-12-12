8e4d3b0770b9386f2566f7d8fb17db3390effafc
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-12-12 15:43:10 +0300

    3.6.3 ready for Marketplace

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -8,16 +8,6 @@ if [ -n "$(apt-cache search azure-security)" ]; then
     apt-get purge -y azure-security
 fi
 
-rm /opt/waf/conf/wsc/snapshots/default.sqlite3 || echo 'wsc snapshot does not exist'
-rm /opt/waf/conf/wsc/config.sqlite3 || echo 'wsc snapshot does not exist'
-
-sed -i "s@LICENSE_SERVER_REST_API.*@LICENSE_SERVER_REST_API = 'https://ptaf-license.ptsecurity.com/api/v3'@g" \
-        /opt/waf/python/lib/python2.7/site-packages/ui/config.py
-
-
-sed -i "s@/opt/waf/artifacts/.*@ @g" /etc/rc.local
-
-
 
 WSC_MGMT_INTERFACE=eth0 WSC_WAN_INTERFACE=eth0 WSC_LAN_INTERFACE=eth1 \
 /usr/local/bin/wsc -e <<EOF
